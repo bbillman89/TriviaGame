0035484a47a1503e9f76ba961829bc7aebec70e3
Mtnman <billman.designs@gmail.com>
2018-11-26 16:04:58 -0500

    at work update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -68,23 +68,78 @@ const trivia = [
 
 let currentQuestion = "";
 let start = false;
+let time = 30;
+let intervalTime;
 
-//Loop through trivia object to separate questions and answers. Then store them in new currentQuestion var.
-for (let i = 0; i < trivia.length; i++) {
-    currentQuestion = trivia[i];
-}
+$(document).ready(function() {
 
-//Start Game by clicking button
-//$("#startBTN").on("click", function(){
-$(document).ready(function(){
+    //Loop through trivia object to separate questions and answers. Then store them in new currentQuestion var.
+    for (let i = 0; i < trivia.length; i++) {
+        currentQuestion = trivia[i];
+    }
 
-    // start = true;
+    //How do we loop through the answers of the currentQuestion?
+    /*for (let n = 0; n < currentQuestion.answers; n++) {
+    var answerBtn = $("<button>");
+    var currentAnswers = currentQuestion.answers[n];
+    console.log(currentAnswers);
+    }*/
 
-    $("#question").text(currentQuestion.question);
-    $("#question").append("<button></button>");
+
+    $("#start-btn").on("click", function(){
+
+        $("#start-btn").hide();
+
+        $("#display-question").text(currentQuestion.question);
+
+        $("#display-time").text("Time remaining " + time);
+
+        runTimer();
+
+    });
+
+    console.log(currentQuestion);
+
+    //------- Timer Functions Start -------//
+    //Sets the pace of the timer. 1 second in this case.
+    function runTimer() {
+        clearInterval(intervalTime);
+        intervalTime = setInterval(decrementTime, 1000);
+    }
+    //Tells the time variable to decrease and sets the display on the html.
+    function decrementTime() {
+        time--;
+        $("#display-time").text("Time remaining " + time);
+        //Stops the timer.
+        if (time === 0) {
+            stopTimer();
+        }
+    }
+    //Tells the interval to stop.
+    function stopTimer () {
+        clearInterval(intervalTime);
+    }
+    //------- Timer Functions End -------//
 
 });
 
-console.log(currentQuestion);
-console.log(currentQuestion.answers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
